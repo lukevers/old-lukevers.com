b322c7391389c024e0e1f1fa4aeebcb56ab53ed7
Luke Evers <me@lukevers.com>
2013-10-31 17:17:08 -0400

    Navbar, auto-slider and other small changes.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	 // Parallax
 	 $.stellar({
 	 	 horizontalScrolling: false,
@@ -35,6 +36,38 @@ $(document).ready(function() {
 	 	 $('#three').toggleClass('fade active');
 	 });
 
+	 // Navbar background
+	 $(window).scroll(function() {
+	 	if ($(window).scrollTop() >= 200)
+	 		$('.navbar').css('background-color', 'rgba(0, 0, 0, .8');
+	 	else $('.navbar').css('background-color', 'rgba(0, 0, 0, 0');
+	 });
+
 	 // Auto-slider
+	 setInterval(function() {
+	 	var id = $('.active').attr('id');
+	 	switch (id) {
+	 		case "one":
+	 			$('.photo2').fadeIn();
+	 			$('.photo').fadeOut(500);
+	 			$('#'+id).toggleClass('fade active');
+	 		 	$('#two').toggleClass('fade active');
+	 			break;
+	 		case "two":
+	 			$('.photo3').show();
+	 			$('.photo, .photo2').fadeOut(500);
+	 			$('#'+id).toggleClass('fade active');
+	 		 	$('#three').toggleClass('fade active');
+	 			break;
+	 		case "three":
+	 			$('.photo').fadeIn();
+	 			$('#'+id).toggleClass('fade active');
+	 		 	$('#one').toggleClass('fade active');
+	 			break;
+	 		default:
+	 			// if this happens, someone fucked shit up
+	 			break;
+	 	}
+	 }, 4000);
 
 });
