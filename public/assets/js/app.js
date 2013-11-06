b41bbd8cbfe94132766184da8eb8db2d8c671e30
Luke Evers <me@lukevers.com>
2013-11-06 10:00:19 -0500

    Overhall

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,73 +1,19 @@
 $(document).ready(function() {
+	$(window).resize(function(){
+		$('.logo').css({
+			position:'absolute',
+			left: ($(window).width() 
+			- $('.logo').outerWidth())/2,
+			top: ($(window).height() 
+			- $('.logo').outerHeight())/2
+		});	
+	});
+	$(window).resize();
+	$(window).resize();
 
-	 // Parallax
-	 $.stellar({
-	 	 horizontalScrolling: false,
-    	 verticalOffset: 0
-     });
-	 
-	 // Slider controls
-	 $('#one').bind('click', function() {
-	 	 var id = $('.active').attr('id');
-
-	 	 $('.photo').fadeIn();
-	 	 
-	 	 $('#'+id).toggleClass('active');
-	 	 $('#one').toggleClass('active');
-	 });
-
-	 $('#two').bind('click', function() {
-	 	 var id = $('.active').attr('id');
-
-	 	 $('.photo2').fadeIn();
- 	 	 $('.photo').fadeOut(500);
-	 	 
-	 	 $('#'+id).toggleClass('active');
-	 	 $('#two').toggleClass('active');
-	 });
-
-	 $('#three').bind('click', function() {
-	 	 var id = $('.active').attr('id');
-	 
- 	 	$('.photo3').show();
- 	 	$('.photo, .photo2').fadeOut(500);
-
-	 	 $('#'+id).toggleClass('active');
-	 	 $('#three').toggleClass('active');
-	 });
-
-	 // Navbar background
-	 $(window).scroll(function() {
-	 	if ($(window).scrollTop() >= 200)
-	 		$('.navbar').css('background-color', 'rgba(0, 0, 0, .8');
-	 	else $('.navbar').css('background-color', 'rgba(0, 0, 0, 0');
-	 });
-
-	 // Auto-slider
-	 setInterval(function() {
-	 	var id = $('.active').attr('id');
-	 	switch (id) {
-	 		case "one":
-	 			$('.photo2').fadeIn();
-	 			$('.photo').fadeOut(500);
-	 			$('#'+id).toggleClass('active');
-	 		 	$('#two').toggleClass('active');
-	 			break;
-	 		case "two":
-	 			$('.photo3').show();
-	 			$('.photo, .photo2').fadeOut(500);
-	 			$('#'+id).toggleClass('active');
-	 		 	$('#three').toggleClass('active');
-	 			break;
-	 		case "three":
-	 			$('.photo').fadeIn();
-	 			$('#'+id).toggleClass('active');
-	 		 	$('#one').toggleClass('active');
-	 			break;
-	 		default:
-	 			// if this happens, someone fucked shit up
-	 			break;
-	 	}
-	 }, 5000);
-
+	// Parallax
+	$.stellar({
+		horizontalScrolling: false,
+		verticalOffset: 0
+	});
 });
